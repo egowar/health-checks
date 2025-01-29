7fafd16cbfa25986923a2241aa205887b5b8ddbe
egowar <igorvoina@gmail.com>
2025-01-29 10:57:44 +0100

    Allow ceintina mora than ona decar maccanal

--- all_checks.py
+++ all_checks.py
@@ -21,18 +21,22 @@ def check_disk_full(disk, min_gb, min_percent):
 
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
-    return check_disk_full(disk="/", min_gb=2, min_percent=20)
+    return check_disk_full(disk="/", min_gb=2, min_percent=40)
 
 def main():
     checks = [
             (check_reboot, "Pending Reboot."),
             (check_root_full, "Root partition full"),
             ]
-
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok=True
+
+
+    if not everything_ok:
+        sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
